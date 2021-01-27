fac46888e4751e5935ed51aea65952979c5163eb
Nate Sales <nate@natesales.net>
2021-01-27 10:42:22 -0800

    add comments and type annotations

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from os import environ, urandom
-from flask import Flask, request, redirect, session
+from flask import Flask, request, redirect, session, Response, jsonify
 from requests_oauthlib import OAuth2Session
 import logging
 from rich.logging import RichHandler
@@ -17,6 +17,7 @@ if not pdb_client_secret:
     log.fatal("PDB_CLIENT_SECRET environment variable is not defined")
     exit(1)
 
+# PeeringDB OAuth URL constants
 pdb_auth_url = "https://auth.peeringdb.com/oauth2/authorize/"
 pdb_token_url = "https://auth.peeringdb.com/oauth2/token/"
 pdb_profile_url = "https://auth.peeringdb.com/profile/v1"
@@ -25,8 +26,12 @@ app = Flask(__name__)
 app.secret_key = urandom(32)
 
 
-@app.route("/")
-def login():
+@app.route("/login")
+def login() -> Response:
+    """
+    Log a user in through PeeringDB redirect
+    :return:
+    """
     peeringdb = OAuth2Session(pdb_client_id)
     authorization_url, state = peeringdb.authorization_url(pdb_auth_url)
     session["oauth_state"] = state
@@ -34,11 +39,15 @@ def login():
 
 
 @app.route("/auth/redirect")
-def callback():
+def callback() -> Response:
+    """
+    Handle inbound PeeringDB redirect and validate user account
+    :return:
+    """
     peeringdb = OAuth2Session(pdb_client_id, state=session["oauth_state"])
     peeringdb.fetch_token(token_url=pdb_token_url, authorization_response=request.url.replace("http", "https"), client_secret=pdb_client_secret, client_id=pdb_client_id)
     print(peeringdb.get(pdb_profile_url).json())
-    return "hi"
+    return jsonify({"auth": True})
 
 
 app.run()
