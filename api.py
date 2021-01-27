61d38deb78f2452e7a735325309b75699b2e5678
Nate Sales <nate@natesales.net>
2021-01-27 10:54:20 -0800

    add mongodb uri

--- api.py
+++ api.py
@@ -1,22 +1,32 @@
 from os import environ, urandom
 from typing import Optional
+import logging
 
 from flask import Flask, request, redirect, session, Response, jsonify
 from requests_oauthlib import OAuth2Session
-import logging
 from rich.logging import RichHandler
+from rich.console import Console
+
+from pymongo import MongoClient
 
 logging.basicConfig(level="NOTSET", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()])
 log = logging.getLogger("rich")
 log.info("Starting PeerlockDB API")
+console = Console()
 
 pdb_client_id = environ.get("PDB_CLIENT_ID")
-pdb_client_secret = environ.get("PDB_CLIENT_SECRET")
 if not pdb_client_id:
     log.fatal("PDB_CLIENT_ID environment variable is not defined")
     exit(1)
+
+pdb_client_secret = environ.get("PDB_CLIENT_SECRET")
 if not pdb_client_secret:
     log.fatal("PDB_CLIENT_SECRET environment variable is not defined")
+    exit(1)
+
+mongo_uri = environ.get("MONGO_URI")
+if not mongo_uri:
+    log.fatal("MONGO_URI environment variable is not defined")
     exit(1)
 
 # PeeringDB OAuth URL constants
@@ -26,6 +36,8 @@ pdb_profile_url = "https://auth.peeringdb.com/profile/v1"
 
 app = Flask(__name__)
 app.secret_key = urandom(32)
+
+db = MongoClient(mongo_uri)["peerlockdb"]
 
 
 def _resp(success: bool, message: str, data: Optional[object] = None) -> Response:
